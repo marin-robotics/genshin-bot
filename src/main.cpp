70b1ea849e36bbb1e9795ca8d102242cd7cf7db2
MarinRobotics <marinrobotics@gmail.com>
2024-02-22 15:28:41 -0800

    Changed ports, various added comments, etc

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,8 @@ using pros::E_CONTROLLER_ANALOG_LEFT_Y;
 using pros::E_CONTROLLER_ANALOG_RIGHT_Y;
 using namespace std;
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+
 // Custom Variables
   // Drive Controls
 bool drive_plus_turning = true; 
@@ -47,16 +49,15 @@ float y_current, x_current, y_direction, x_direction;
 float y_true_step;
 float x_true_step;
 
-pros::Motor front_left_motor(17, pros::E_MOTOR_GEAR_BLUE, true,
-                             pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor back_left_motor(16, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor front_right_motor(19, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor back_right_motor(20, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor catapult_motor_left(1, pros::E_MOTOR_GEAR_RED, false, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor catapult_motor_right(8, pros::E_MOTOR_GEAR_RED, true, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor snarf_rotator_right( 2,pros::E_MOTOR_GEAR_GREEN, false, pros:: E_MOTOR_ENCODER_DEGREES );
-pros::Motor snarf_rotator_left( 6,pros::E_MOTOR_GEAR_GREEN, true, pros:: E_MOTOR_ENCODER_DEGREES );
-pros::Motor snarf_driver(4, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor front_left_motor(7, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor back_left_motor(8, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor front_right_motor(5, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor back_right_motor(6, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor catapult_motor_left(10, pros::E_MOTOR_GEAR_RED, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor catapult_motor_right(9, pros::E_MOTOR_GEAR_RED, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor snarf_rotator_right(1,pros::E_MOTOR_GEAR_GREEN, false, pros:: E_MOTOR_ENCODER_DEGREES);
+pros::Motor snarf_rotator_left(2,pros::E_MOTOR_GEAR_GREEN, true, pros:: E_MOTOR_ENCODER_DEGREES );
+pros::Motor snarf_driver(11, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_DEGREES);
 //grouping both sides of the drivetrain
 pros::MotorGroup left_motors({front_left_motor, back_left_motor});
 pros::MotorGroup right_motors({front_right_motor, back_right_motor});
@@ -69,14 +70,10 @@ float modifier = 600.0/127;
 float rt2 = sqrt(2.0);
 //defining the controller
 int auton_step = 0; // debug
-int catainit =120; // side one is defensive, side -1 is offensive
-float autonselect=   1;
-void debug_auton(int step){
-  pros::screen::print(pros::E_TEXT_LARGE_CENTER, 1, "Running Auton");
-  pros::screen::print(pros::E_TEXT_MEDIUM, 3, "Step: %d", step);
-}
+int catainit =120; //defined as amount that catapult goes down on initilization- arc of catapult.
 
-float extenddistance = 250;
+
+float extenddistance = 250; //amount that motors driving snarfer rotate upon extension
 bool isextended = false;
 bool snarferon = false;
 void on_center_button() {
@@ -89,27 +86,26 @@ void on_center_button() {
 	} 
 }
 float move_factor= 360/Wheel_Circumference;
+//changes the position of the snarfer. Uses toggle function to move snarfer to its "up" or "down" position based on current value of isextended. False= up, true=down. If the snarfer is not working, try making the value of isextended negative or reversing the value of extenddistance.
 void snarf_turn(){
-            if (isextended == true) {
-              snarf_rotator.move_relative(extenddistance, 127);
-              isextended = false;
-              pros::delay(20);
-            } 
-            else if (isextended == false) {
-              snarf_rotator.move_absolute(-extenddistance, 127);
-              isextended = true;
-              pros::delay(20);
-            }
-
-            else {
-              pros::delay(20);
-            }
+  if (isextended == true) {
+    snarf_rotator.move_relative(extenddistance, 127);
+    isextended = false;
+    pros::delay(20);
+  } 
+  else if (isextended == false) {
+    snarf_rotator.move_absolute(-extenddistance, 127);
+    isextended = true;
+    pros::delay(20);
+  }
+  else {
+    pros::delay(20);
+  }
 }
-
-
-pros::Mutex action;
-// inches, rpm, defaults to forward
-void move( float inches, float velocity) { 	//action.take(1000);
+// changes autonomous side if the robot is running irregular code, it's likely this: side one is defensive, side -1 is offensive
+float autonselect= -1;
+// moves the robot in a linear manner with parameters: inches, rpm, moves forward assuming that the movement is positive
+void move( float inches, float velocity) { 
 	Forward_Wheel_Rotation = Move_Tuning_Factor*(inches/Wheel_Circumference)*360;
 
   left_motors.tare_position();
@@ -117,19 +113,17 @@ void move( float inches, float velocity) { 	//action.take(1000);
 
   left_motors.move_relative(Forward_Wheel_Rotation,velocity);
   right_motors.move_relative(Forward_Wheel_Rotation,velocity);
-		//action.give();
-	}
-  // inputs in degrees, rpm, defaults to clockwise
+  }
+// turns the robot around its wheelbase: takes angle in degrees, velocity in RPM. turns clockwise from a positive input assuming defensive side auton. Reverses itself for offensive side autonomous
 void turn (float angle, float velocity) {
-	//action.take(1000);
+	//action.take(1000); 
 	 Turning_Distance = angle/360 * Turning_Circumference;
   Wheel_Revolutions = Turning_Distance/Wheel_Circumference;
   Turn_Wheel_Rotation = Turn_Tuning_Factor*Wheel_Revolutions*360; // Degrees that the wheel should turn
   left_motors.move_relative(Turn_Wheel_Rotation*autonselect, velocity);
   right_motors.move_relative(-Turn_Wheel_Rotation*autonselect, velocity);
-
 }
-void timedmove (float inches, float velocity, float milliseconds) {
+/*void timedmove (float inches, float velocity, float milliseconds) {
  
   Forward_Wheel_Rotation = Move_Tuning_Factor*(inches/Wheel_Circumference)*360;
 float distance;
@@ -147,7 +141,7 @@ float time;
   }
   
 
-}
+} */
 //change to negative if on right
 void initialize() {
 	
@@ -160,107 +154,117 @@ void initialize() {
 void disabled() {}
 
 void competition_initialize() {}
-
+// before altering this code, go read the comments for the move and turn functions
 void autonomous() {
-  autoncheck=true;
-  catapult_motor.set_brake_modes(MOTOR_BRAKE_HOLD);
-  	snarf_rotator_right.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-	snarf_rotator_left.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
- 
+  //check autonselect as defined above to ensure auton is working
+  //comments for move and turn functions above
+// autoncheck ensures that autonomous has run to make sure that the loop at the beginning of driver controlled functions
+autoncheck=true;
+//sets braking to prevent drift
+catapult_motor.set_brake_modes(MOTOR_BRAKE_HOLD);
+snarf_rotator_right.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+snarf_rotator_left.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+//responsible for moving robot into alignment with goal
 move(-30, 400);
 pros::delay(800);
-turn(50, 600);
-
+turn(-50, 600);
 pros::delay(500);
-  left_motors.move_velocity(-600);
-  right_motors.move_velocity(-600);
-  pros::delay(700);
- left_motors.move_velocity(0);
-  right_motors.move_velocity(0);
-//pros::Task body1{ 
-//  [=]{
-  //  action.take(5000);
-  move(4, 600);
-  pros::screen::print(pros::E_TEXT_MEDIUM, 1,
-                              "one");
-  pros::delay(500);
-  
-  turn(-45,500);
-  pros::screen::print(pros::E_TEXT_MEDIUM, 1, "two");
- 
-  pros::delay(600);
-  //moves to centre of elevation bar
+// rams triball into goal
+left_motors.move_velocity(-600);
+right_motors.move_velocity(-600);
+pros::delay(700);
+left_motors.move_velocity(0);
+right_motors.move_velocity(0);
+move(4, 400);
+turn (-90,600);
+
+/*//moving robot from goal to match load bar and aligning it
+move(4, 600);
+pros::delay(500);
+//turns robot to be parallel with match load bar
+turn(-45,500);
+pros::delay(600);
+  //moves to centre of elevation bar- necessary for aligning with triball
   move(17, 600);
-  pros::screen::print(pros::E_TEXT_MEDIUM, 1,
-                              "three");
  pros::delay(700);
- //tweak this turn as needed at tournament -aligns with match load bar
-  turn(105, 600);
+ //tweak this turn as needed at tournament-aligns robot with match load bar and center triball
+  turn(120, 600);
   pros::delay(500);
+  //backs into bar: velocity and voltage limits set to push robot flush with bar without going over it. If the robot is going over the bar reduce the power limit, if it is not being pushed against the bar increase it.
   left_motors.move_velocity(300);
   right_motors.move_velocity(300);
+  //voltage limits as mentioned above
   left_motors.set_voltage_limit(4000);
   right_motors.set_voltage_limit(4000);
-  pros::delay(900);
-  
-    left_motors.move_velocity(0);
-  right_motors.move_velocity(0);
-    left_motors.set_voltage_limit(12000);
-  right_motors.set_voltage_limit(12000);
-  pros::delay(600);
-  pros::screen::print(pros::E_TEXT_MEDIUM, 1, "four");
-  snarf_turn(); 
-  pros::delay(600);
-  catapult_motor.move_relative(235 , 127);
-  pros::delay(500);
-  snarf_driver.move_velocity(600);
-  pros::delay(700);
-  	snarf_rotator_right.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-	snarf_rotator_left.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-   move(-6, 600);
-   pros::delay(800);
+  //delay essential> do not reduce more than a small amount
+  pros::delay(900); 
+  //resets power limits to default, stops robot from pushing against match load bar
+left_motors.move_velocity(0);
+right_motors.move_velocity(0);
+left_motors.set_voltage_limit(12000);
+right_motors.set_voltage_limit(12000);  
+pros::delay(600);
+//deploys and runs snarfer while priming catapult
+snarf_turn(); 
+pros::delay(600);
+//catapult is primed- if not functioning properly consistently check that the slip gears are just barely touching the above gears
+catapult_motor.move_relative(235 , 127);
+pros::delay(500);
+//snarfer starts running
+snarf_driver.move_velocity(600);
+pros::delay(700);
+snarf_rotator_right.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+snarf_rotator_left.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+move(-6, 600);
+pros::delay(800);
+//runs autonomous function specific to defensive side
 if(autonselect==1) {
   turn(25, 400);
    pros::delay(1200);
-   catapult_motor.move_relative(-45,127);
+   catapult_motor.move_relative(45,127);
    pros::delay(300);
    catapult_motor.move_relative(200, 127);
-  pros::delay(800);
-  //tweak this turn and move at tourney- supposed to get robot into position where the following turn will allow the snarfer to be aligned with the center triball
+  pros::delay(300);
+  //following block of code is responsible for moving the robot away from the match load bar.
+  //tweak this turn and move at tourney- supposed to get robot into position where the following turn will allow the snarfer to be able to move into channel with elevation bars
    turn(-135, 400);
-      pros::delay(800);
-    snarf_turn();
-   snarf_driver.move_velocity(0);
+  pros::delay(800);
+  snarf_turn();
+  snarf_driver.move_velocity(0);
    pros::delay(500);
-    move(23, 600);
+    move(21, 600);
    pros::delay(600);
-  turn(-40, 600);
+   //aligns robot to be parallel with short bars> tweak as needed at tournament as this turn has a tendency to get robot stuck
+  turn(-30, 600);
    pros::delay(500);
    snarf_turn();
-   //go forward and touch the bar
+   //go forward and touch the bar: reduce if the robot is touching the other side of the field tiles to avoid disqualification
    snarf_driver.move_velocity(-600);
-  move(20,600);
+  move(16,600);
   pros::delay(600);
-
-  
-     left_motors.set_voltage_limit(5000);
+  //makes sure the robot does not ram into the bar by having it back in slowly
+  left_motors.set_voltage_limit(5000);
   right_motors.set_voltage_limit(5000);
-
   move(5, 250);
     }
     //12.7 seconds 
+//runs offensive side autonomous
 else if (autonselect==-1) {
-  move(-6, 600);
-    turn(-25, 400);
+  //turns robot and fires loaded triball to score two points
+  move(-4, 600);
+    turn(-40, 400);
    pros::delay(1000);
+   //fires catapult
    catapult_motor.move_relative(540, 127);
 }
+*/
+  //setup for driver controlled> do not touch
   front_left_motor.set_brake_mode(MOTOR_BRAKE_HOLD);
 	back_left_motor.set_brake_mode(MOTOR_BRAKE_HOLD);
 	front_right_motor.set_brake_mode(MOTOR_BRAKE_HOLD);
   back_right_motor.set_brake_mode(MOTOR_BRAKE_HOLD);
 	catapult_motor.set_brake_modes(MOTOR_BRAKE_HOLD); 
-     left_motors.set_voltage_limit(12000);
+  left_motors.set_voltage_limit(12000);
   right_motors.set_voltage_limit(12000);
 
 }
@@ -279,9 +283,10 @@ void opcontrol() {
 	catapult_motor.set_brake_modes(MOTOR_BRAKE_HOLD);
 	snarf_rotator_right.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 	snarf_rotator_left.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
- if (autoncheck) {catapult_motor.move_relative(370, 200);
+  //post auton driver controlled setup- add as needed
+/* if (autoncheck) {catapult_motor.move_relative(370, 200);
  pros::delay(500);
-}
+} */
      snarf_driver.move_velocity(0);
 	//gets the x and y inputs from both controller sticks
         
@@ -295,7 +300,7 @@ void opcontrol() {
                               right_y, right_x);
         
         
-if (drive_plus_turning) { // Option to use the right stick for turning
+    if (drive_plus_turning) { // Option to use the right stick for turning
       if (abs(right_x) < abs(left_x)) {
         right_x = left_x;
       }
@@ -305,14 +310,14 @@ if (drive_plus_turning) { // Option to use the right stick for turning
         right_y = left_y;
       }
     }
-
+/*
     // Drive Control Loop (LEFT)
     y_direction = sgn(left_y);
     x_direction = sgn(right_x);
-
+*/
     float y_goal = powf((abs(left_y) / 127), 2) * 127;
     float x_goal = powf((abs(right_x) / 127), 2) * 127;
-    
+ /*   
     float y_error = y_goal - y_current;
     float x_error = x_goal - x_current;
 
@@ -336,13 +341,13 @@ if (drive_plus_turning) { // Option to use the right stick for turning
     }
     y_current += y_true_step;
     x_current += x_true_step;
-
+*/ // standard one stick drive code- gets x and y direction and moves motors accordingly
     if (standard_drive) {
-      left_motors.move((y_current * y_direction) + (turn_constant * x_current * x_direction));
-      right_motors.move((y_current * y_direction) - (turn_constant * x_current * x_direction));
+      left_motors.move((y_goal * y_direction) + (turn_constant * x_goal * x_direction));
+      right_motors.move((y_goal * y_direction) - (turn_constant * x_goal * x_direction));
     } else {
-      left_motors.move((y_current * y_direction) - (turn_constant * x_current * x_direction));
-      right_motors.move((y_current * y_direction) + (turn_constant * x_current * x_direction));
+      left_motors.move((y_goal * y_direction) - (turn_constant * x_goal * x_direction));
+      right_motors.move((y_goal * y_direction) + (turn_constant * x_goal * x_direction));
     }
 
     pros::delay(20);
@@ -350,7 +355,7 @@ if (drive_plus_turning) { // Option to use the right stick for turning
          
           
           
-          // snarfer code
+          //code to make the motor snarf or barf depending on which button is pressed (will comment which button is which when the snarfer is on)
 
   if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
             snarf_driver.move_velocity(600);
@@ -361,18 +366,20 @@ if (drive_plus_turning) { // Option to use the right stick for turning
   else {
             snarf_driver.move_velocity(0);
   }
-          // forebar code- if not working reverse input and output
+          // button control for snarf turn as defined above
    if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1)) {
       snarf_turn();
           }
-          // catapult code
+          // catapult code, just has the catapult turn a full revolution
 
 if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)) {
    catapult_motor.move_relative(540, 127); //pos was 540
   }
           pros::delay(20);	
+          //various calibration functions for the catapult, will either move it slightly up or down or will move it the amount defined by the arc
 if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_LEFT)){
-   catapult_motor.move_relative(45 , 127);
+   catapult_motor_left.move_relative(45 , 127);
+   catapult_motor_right.move_relative(45 , 127);
 }
 if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_RIGHT)){
    catapult_motor.move_relative(-45 , 127);}
@@ -382,6 +389,8 @@ if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B)) {
 if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP)){
    catapult_motor.move_relative(catainit , 127);
 }
+
+  //forcibly retracts the snarfer regardless of its current position.
   if (master.get_digital(pros::E_CONTROLLER_DIGITAL_Y)) {
     snarf_rotator.move_absolute(0, 200);
   }
